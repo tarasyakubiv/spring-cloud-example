a5532d4b93cf9630619bbd4a7d36daca30cd9c82
Taras Yakubiv <tarasyakubiv@localhost.localdomain>
2018-11-18 00:00:53 -0600

    Cleanup

--- travis_scripts/build_services.sh
+++
@@ -1,2 +0,0 @@
-echo "Building with travis commit of $BUILD_NAME ..."
-mvn clean package docker:build
--- travis_scripts/deploy_to_docker_hub.sh
+++
@@ -1,8 +0,0 @@
-echo "Pushing service docker images to docker hub ...."
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push tarasyakubiv/tmx-authentication-service:$BUILD_NAME
-docker push tarasyakubiv/tmx-licensing-service:$BUILD_NAME
-docker push tarasyakubiv/tmx-organization-service:$BUILD_NAME
-docker push tarasyakubiv/tmx-confsvr:$BUILD_NAME
-docker push tarasyakubiv/tmx-eurekasvr:$BUILD_NAME
-docker push tarasyakubiv/tmx-zuulsvr:$BUILD_NAME
